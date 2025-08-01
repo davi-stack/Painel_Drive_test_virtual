ee76cc0a8860d5377380400f160d73fa197c7724
davi-stack <davifariaroch@gmail.com>
2025-08-01 15:36:05 -0300

    producao

--- servidor.py
+++ servidor.py
@@ -52,4 +52,4 @@ def servir_arquivo(arquivo):
 
     return send_file(caminho)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
